86c89fd4d5e37cbc8ee5bf07c09159eb46681198
Carlos Francis Garcia <carlos.garcia@student.adelaide.edu.au>
2025-02-14 15:39:22 +1030

    Update passwordMeter.js
    I forgot to add in the boolean variable 'specials' to confirm if a special character(s) exists from the user password input. Also worked on shortening the 'else if' statement for determining special characters by making a string variable 'specialChars' that contains all the special characters from the English keyboard. The new if-statement then uses the current character in the password string in the  for-loop to see if the current iterated character exists within the string 'specialChars'. Thus shortening the if-statement's condition.
    
    Also changed how the program acquires the ascii values of each element by using '.charCodeAt()' function instead of directly acquiring elements from string. This way, the if-statements really do check the ascii values since the function '.charCodeAt()' returns a numeric value instead of an character element.

--- passwordMeter.js
+++ passwordMeter.js
@@ -4,57 +4,30 @@ var result = document.getElementById("result");
 
 document.querySelector("#password").addEventListener("input", strengthChecker);
 
+var specialChars = '!@#$%^&*()-_=+[{}]\\|,.<>/?;:\'\"';
+
 function strengthChecker() {
 
   var password_string = password_to_check.value;
   var lowercase = false;
   var uppercase = false;
   var numbers = false;
+  var specials = false;
 
   for ( let index = 0; index < password_string.length; index++ ) {
 
-    if ( password_string[index] > 96 &&  password_string[index] < 123 ) {
+    if ( password_string.charCodeAt(index) > 96 &&  password_string.charCodeAt(index) < 123 ) {
         lowercase = true;
-    } else if ( password_string[index] > 64 &&  password_string[index] < 91 ) {
+    } else if ( password_string.charCodeAt(index) > 64 &&  password_string.charCodeAt(index) < 91 ) {
         uppercase = true;
-    } else if ( password_string[index] > 47 &&  password_string[index] < 58 ) {
+    } else if ( password_string.charCodeAt(index) > 47 &&  password_string.charCodeAt(index) < 58 ) {
         numbers = true;
-    } else if ( password_string[index] == '@' 
-            ||  password_string[index] == '#'
-            ||  password_string[index] == '$'
-            ||  password_string[index] == '%'
-            ||  password_string[index] == '^'
-            ||  password_string[index] == '&'
-            ||  password_string[index] == '*'
-            ||  password_string[index] == '('
-            ||  password_string[index] == ')'
-            ||  password_string[index] == '~'
-            ||  password_string[index] == '`'
-            ||  password_string[index] == '?'
-            ||  password_string[index] == '/'
-            ||  password_string[index] == '\\'
-            ||  password_string[index] == '.'
-            ||  password_string[index] == ','
-            ||  password_string[index] == '<'
-            ||  password_string[index] == '>'
-            ||  password_string[index] == ':'
-            ||  password_string[index] == ';'
-            ||  password_string[index] == '\''
-            ||  password_string[index] == '"'
-            ||  password_string[index] == '{'
-            ||  password_string[index] == '}'
-            ||  password_string[index] == '['
-            ||  password_string[index] == ']'
-            ||  password_string[index] == '-'
-            ||  password_string[index] == '_'
-            ||  password_string[index] == '+'
-            ||  password_string[index] == '='
-            ||  password_string[index] == '|' ) {
-            specials = true;
-        } else if ( password_string[index] == ' ' ) {
-            result.innerHTML = "Invalid!";
-            return;
-        }
+    } else if ( specialChars.includes(password_string[index]) ) {
+        specials = true;
+    } else if ( password_string[index] == ' ' ) {
+        result.innerHTML = "Invalid!";
+        return;
+    }
   }
 
   var points = BigInt(26 * uppercase + 26 * lowercase + 10 * numbers + 31 * specials) ** BigInt(password_string.length);
